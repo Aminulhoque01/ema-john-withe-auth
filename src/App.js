c8c7cf275bc89619bab60577c1e50c3ec655aaee
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-03 10:55:09 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       children: [
         {
           path: '/',
-          loader: () => fetch('products.json'),
+          
           element: <Shop></Shop>
         },
         {
